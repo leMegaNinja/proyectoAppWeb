69340525374126892058101170bf0436476c26c7
Angel Lopez <angel92_9@hotmail.com>
2025-01-20 18:26:06 -0600

    header and showing DataFrame

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ import streamlit as st
 
 # lectura de datos
 cars_data = pd.read_csv('vehicles_us.csv')
-print(cars_data.head())
 
-# crear un histograma
-fig = px.histogram(cars_data, x="model_year")
-fig.show()
-
-# crear un gráfico de dispersión
+st.header('Listado vehicular')
+st.write(cars_data)
